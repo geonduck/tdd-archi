cce5f7d3088fd78c5df93908810e5cb427303ec1
geonduck <geonduck93@gmail.com>
2024-12-22 21:41:04 +0900

    최초 생성

---
+++ src/main/java/com/tddarchi/TddArchiApplication.java
@@ -0,0 +1,13 @@
+package com.tddarchi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddArchiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddArchiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tddarchi/TddArchiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tddarchi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class TddArchiApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
